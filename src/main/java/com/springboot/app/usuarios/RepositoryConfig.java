1fd8fe5f5972113cfb0111a1c8ebca377ff019a7
camilo.diaz <camilo.diaz@sophossolutions.com>
2020-06-23 19:31:08 -0500

    crud repository exposed

---
+++ src/main/java/com/springboot/app/usuarios/RepositoryConfig.java
@@ -0,0 +1,16 @@
+package com.springboot.app.usuarios;
+
+import com.springboot.app.usuarios.models.entity.Role;
+import com.springboot.app.usuarios.models.entity.Usuario;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+  @Override
+  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    config.exposeIdsFor(Usuario.class, Role.class);
+  }
+}
--- src/main/java/com/springboot/app/usuarios/models/dao/UsuarioDao.java
+++ src/main/java/com/springboot/app/usuarios/models/dao/UsuarioDao.java
@@ -2,11 +2,14 @@ package com.springboot.app.usuarios.models.dao;
 
 import com.springboot.app.usuarios.models.entity.Usuario;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioDao extends PagingAndSortingRepository<Usuario, Long> {
 
-  Usuario findByUsername(String username);
+  @RestResource(path="buscar-username")
+  Usuario findByUsername(@Param("nombre") String username);
 
 }
